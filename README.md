99464b5c886faf716eaa7024672b828a295fe103
glasscat82 <glasscat82@gmail.com>
2021-06-26 22:55:29 +0200

    best20

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # simple class for kinopoisk
 parsing kinopoisk for Russia
+
+## Program launch /best20.py
+tables data output the PrettyTable module 
+
+![best20](https://github.com/glasscat82/kino/blob/master/img/001.png "best20")
--- best20.py
+++ best20.py
@@ -1,6 +1,6 @@
 # parsing kinopoisk.ru for Russia
 from datetime import datetime
-# from prettytable import PrettyTable
+from prettytable import PrettyTable
 from kino import kino
 
 def main():
@@ -9,11 +9,15 @@ def main():
 	# ----- 20 best films
 	url_ = 'https://www.kinopoisk.ru/lists/navigator/?quick_filters=high_rated&limit=20&tab=best'
 	flm = kino(url=url_, filename='kino.json')
-	html_ = flm.get_html()
-	# flm.p(html_)
+	html_ = flm.get_html()	
 	links_ = flm.get_all_links(html=html_)
-	for l_ in links_['films']:		
-		flm.p(l_)
+	# the table
+	x = PrettyTable()
+	x.field_names = ["№", "Название", "Год", "Страна", "Рейтинг", "Голосов"]
+	x.align["Название"] = x.align["Год"] = x.align["Страна"] = "l"
+	for index, r_ in enumerate(links_['films'], 1):		
+		x.add_row([index, r_[2], r_[3], r_[4], r_[5], r_[6]])
+	print(x.get_string(title='Лучшие 20 фильмов'))
 	# -----
 
 	end = datetime.now()
